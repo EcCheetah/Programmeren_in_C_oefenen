04c8407c7f84a535ef9a16ce8b2e052f35162077
EcCheetah <98852126+EcCheetah@users.noreply.github.com>
2022-02-01 23:32:02 +0100

    Add files via upload

---
+++ index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// int uitkomst[100];
+
+// int main (int argc, char **argv[]) {
+//     for (int b = 2; b <= 100; b++) {
+//         int counter = 0;
+
+//         for (int a = 0; a <= 100; a++) {
+//             if (a % b == 0) {
+//                 uitkomst[counter] = a;
+//                 counter++;
+//             }
+//         }
+
+//         printf("Deze waardes zijn deelbaar door %d: ", b);
+//         for (int c = 0; c < counter; c++) {
+//             printf("| %d |", uitkomst[c]);
+//         }
+//         printf("\n\n");
+//     }
+
+//     return 0;
+// }
